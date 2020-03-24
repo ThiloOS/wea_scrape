1aea457ddef8b9761327a99bdc311008ee969837
ThiloOS <sebastian.thielemann@hotmail.de>
2020-03-25 00:35:25 +0100

    json file output

--- wea_scrape.py
+++ wea_scrape.py
@@ -1,10 +1,20 @@
-import requests
+import requests, re, json
 from bs4 import BeautifulSoup
-import re
-import textwrap
 
-URL = 'https://de.wikipedia.org/wiki/Liste_von_Windkraftanlagen_in_Bremen,_Hamburg_und_Niedersachsen'
+print()
+URL = input('paste url here: ')
 page = requests.get(URL)
+print()
+
+
+if page.status_code == 200:
+    print('success. status code ' + str(page.status_code))
+else:
+    print('error. status code ' + str(page.status_code))
+print()
+
+file_name_input = input('filename: ')
+file_name = file_name_input + '.txt'
 
 soup = BeautifulSoup(page.content ,'html.parser')
 
@@ -27,12 +37,24 @@ def letters(input):
     return ''.join([i for i in input if not i.isdigit()])
 
 
-# Find built date
+# Format built date
 def built_date(input):
     return input[:4]
 
 
+# Format Location
+def location(input):
+    return re.sub(r"(?<=\w)([A-Z])", r" \1", input)
+
+
+data = []
 for i in range(len(wrapper)-1):
-    #print(letters(wrapper[i+1][0]) + ' ' +built_date(wrapper[i+1][1]))
-    #print(built_date(wrapper[i+1][1]))
-    print(wrapper[i+1][4])
+    dict = {'name':letters(wrapper[i+1][0]), 'built':built_date(wrapper[i+1][1]), 'in_use':True, 'power':wrapper[i+1][2], 'number':wrapper[i+1][3], 'type':wrapper[i+1][4], 'location':location(wrapper[i+1][5]), 'zip':wrapper[i+1][6], 'coordinates':wrapper[i+1][7], 'owner':wrapper[i+1][8]}
+    data.append(dict)
+
+    
+with open(file_name , 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+    print('file created')
+
+   
