fdd2ef41f7ca3f404056e199b5df23c5ce6f5fa2
ThiloOS <sebastian.thielemann@hotmail.de>
2020-03-24 22:13:23 +0100

    Formatting apllied

--- wea_scrape.py
+++ wea_scrape.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import textwrap
 
 URL = 'https://de.wikipedia.org/wiki/Liste_von_Windkraftanlagen_in_Bremen,_Hamburg_und_Niedersachsen'
 page = requests.get(URL)
@@ -16,11 +18,21 @@ for row in table.find_all('tr'):
         wea.append(cell.text.rstrip('\n'))
     wrapper.append(wea)   
 
+
+# Format WEA Names
+# Removing digits & '[' & ']'
+def letters(input):
+    input = input.replace('[', '')
+    input = input.replace(']', '')
+    return ''.join([i for i in input if not i.isdigit()])
+
+
+# Find built date
+def built_date(input):
+    return input[:4]
+
+
 for i in range(len(wrapper)-1):
-    print(wrapper[i+1][9])
-
-
-    
-
-    
-
+    #print(letters(wrapper[i+1][0]) + ' ' +built_date(wrapper[i+1][1]))
+    #print(built_date(wrapper[i+1][1]))
+    print(wrapper[i+1][4])
